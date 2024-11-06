26e83e8db181d9b44ba8e3e6b91db1c9d4fd5e49
Maciejo <maciej.majewski19@op.pl>
2024-11-06 15:08:43 +0100

    Add/get/del

--- helpers/data.js
+++ helpers/data.js
@@ -1,2 +1,5 @@
+import "dotenv/config";
 export const baseUrl = "https://demoqa.com"
 export const userID = "95a0efc5-ce61-42a7-b990-6f8fd23efc0c"
+export const user = "testnode321?"
+export const password = process.env.SECRET_PASSWORD
--- tests/api.test.js
+++ tests/api.test.js
@@ -2,8 +2,9 @@ import { expect } from "chai";
 import "dotenv/config";
 import pkg from "pactum";
 const { spec } = pkg;
-import { baseUrl, userID } from "../helpers/data.js";
+import { baseUrl, userID, user, password } from "../helpers/data.js";
 
+let token_response;
 describe("Api tests", () => {
   it("get request", async () => {
     const response = await spec()
@@ -19,15 +20,63 @@ describe("Api tests", () => {
     expect(r).to.include("Learning JavaScript Design Patterns");
   });
 
-  it.skip("Create a use", async () => {
+  it.skip("Create a user", async () => {
     const response = await spec()
       .post(`${baseUrl}/Account/v1/User`)
       .withBody({
-        userName: "testnode321?",
-        password: process.env.SECRET_PASSWORD,
+        userName: user,
+        password: password,
       })
       .inspect();
     expect(response.statusCode).to.eql(201);
-    //95a0efc5-ce61-42a7-b990-6f8fd23efc0c
+  });
+
+  it.only("Generate token", async () => {
+    const response = await spec()
+      .post(`${baseUrl}/Account/v1/GenerateToken`)
+      .withBody({
+        userName: user,
+        password: password,
+      })
+      .inspect();
+    token_response = response.body.token;
+    expect(response.statusCode).to.eql(200);
+    expect(response.body.result).to.eql("User authorized successfully.");
+  });
+
+  it.only("Check token", async () => {
+    console.log("another it block " + token_response);
+  });
+
+  it.only("Add a book", async () => {
+    const response = await spec()
+      .post(`${baseUrl}/BookStore/v1/Books`)
+      .withBearerToken(token_response)
+      .inspect()
+      .withBody({
+        "userId": userID,
+       "collectionOfIsbns": [
+        {
+            isbn: "9781449325862",
+        }],
+      });
+    expect(response.statusCode).to.eql(201);
+  });
+
+  it.only("Check a book", async () => {
+    const response = await spec()
+      .get(`${baseUrl}/Account/v1/User/${userID}`)
+      .withBearerToken(token_response)
+      .inspect()
+    expect(response.statusCode).to.eql(200);
+  });
+
+  it.only("Delete all book", async () => {
+    const response = await spec()
+      .delete(`${baseUrl}/Account/v1/User/${userID}`)
+      .inspect()
+      .withBearerToken(token_response)
+    expect(response.statusCode).to.eql(204)
+    expect(response.body.books).to.eql([])
   });
 });
