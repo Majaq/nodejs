9bc952ebc29764590fc1697faa9f8b540bb8ad8b
Maciejo <maciej.majewski19@op.pl>
2024-11-06 15:28:24 +0100

    changes

--- helpers/data.js
+++ helpers/data.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
 export const baseUrl = "https://demoqa.com"
-export const userID = "95a0efc5-ce61-42a7-b990-6f8fd23efc0c"
-export const user = "testnode321?"
+export const userID = "bed6d776-093e-4cda-a4bf-c77822e18c20"
+export const user = "Maciejo2"
 export const password = process.env.SECRET_PASSWORD
