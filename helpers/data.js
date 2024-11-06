90bfc6181fe0c4ec51c01301072b60e804b6a32e
Maciejo <maciej.majewski19@op.pl>
2024-11-06 12:28:58 +0100

    login etc

---
+++ helpers/data.js
@@ -0,0 +1,2 @@
+export const baseUrl = "https://demoqa.com"
+export const userID = "95a0efc5-ce61-42a7-b990-6f8fd23efc0c"
--- tests/api.test.js
+++ tests/api.test.js
@@ -1,16 +1,33 @@
 import { expect } from "chai";
+import 'dotenv/config'
 import pkg from "pactum";
 const { spec } = pkg;
+import { baseUrl, userID} from "../helpers/data.js"
 
 describe("Api tests", () => {
   it("get request", async () => {
     const response = await spec()
       .get("https://demoqa.com/BookStore/v1/Books")
       .inspect();
+    const r = JSON.stringify(response.body);
+    console.log(process.env.SECRET_PASSWORD)
     expect(response.statusCode).to.eql(200);
     expect(response.body.books[1].title).to.eql(
       "Learning JavaScript Design Patterns"
     );
     expect(response.body.books[4].author).to.eql("Kyle Simpson");
+    expect(r).to.include("Learning JavaScript Design Patterns");
+  });
+
+  it("Create a use", async () => {
+    const response = await spec()
+      .post(baseUrl + "/Account/v1/User")
+      .withBody({
+        userName: userID,
+        password: process.env.SECRET_PASSWORD,
+      })
+      .inspect();
+        expect(response.statusCode).to.eql(201);
+        //95a0efc5-ce61-42a7-b990-6f8fd23efc0c
   });
 });
